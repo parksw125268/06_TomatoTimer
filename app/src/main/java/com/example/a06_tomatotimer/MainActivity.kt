560ccc28f0e9e35561f9a867fc402eff45dcbc4e
CookDas2 <parksw125368@naver.com>
2021-09-07 23:46:48 +0900

    210907

--- app/src/main/java/com/example/a06_tomatotimer/MainActivity.kt
+++ app/src/main/java/com/example/a06_tomatotimer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.a06_tomatotimer
 
 import android.annotation.SuppressLint
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -19,10 +20,38 @@ class MainActivity : AppCompatActivity() {
     }
     private var currenCountDownTimer: CountDownTimer? = null
 
+    private val soundPool = SoundPool.Builder().build()
+    private var tickingSoundId:Int? = null
+    private var bellSoundId:Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindView()
+        initSounds()//사운드파일 로드
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // soundPool.Resume(tickingSoundId) //사운드id 에 해당하는 사운드를 다시 재생
+        soundPool.autoResume() //모든 사운드 풀의 사운드를 다시 재생
+    }
+
+    override fun onPause() {
+        super.onPause()
+       // soundPool.pause(tickingSoundId) //사운드id 에 해당하는 사운드만 멈춤
+        soundPool.autoPause() //모든 사운드 풀의 사운드를  멈춤
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+
+    private fun initSounds() {
+        //sound들을 메모리에 로드          //soundId를 Int형으로 반환
+        tickingSoundId =  soundPool.load(this, R.raw.timer_ticking,1) //priority는 아무 효과가 없다고 하지만 1을 넣으라고 함.
+        bellSoundId = soundPool.load(this, R.raw.timer_bell,1)
     }
 
     private fun bindView() {
@@ -36,12 +65,14 @@ class MainActivity : AppCompatActivity() {
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
                     currenCountDownTimer?.cancel()
                     currenCountDownTimer = null
+                    tickingSoundId?.let {
+                        soundPool.stop(it)
+                    }
                 }
 
                 override fun onStopTrackingTouch(seekBar:  SeekBar?){
                     seekBar ?:return
-                    currenCountDownTimer = createCountDownTimer(seekBar.progress *60 *1000L)
-                    currenCountDownTimer?.start()
+                    startCountDown()
                 }
             }
         )
@@ -49,8 +80,7 @@ class MainActivity : AppCompatActivity() {
     private fun createCountDownTimer(initialMillis:Long) =
         object :CountDownTimer(initialMillis,1000L)  {
             override fun onFinish() {
-                updateRemainTimes(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
 
             override fun onTick(millisUntilFinished: Long) {
@@ -58,6 +88,24 @@ class MainActivity : AppCompatActivity() {
                 updateSeekBar(millisUntilFinished)
             }
         }
+    private fun startCountDown(){
+        currenCountDownTimer = createCountDownTimer(seekBar.progress *60 *1000L)
+        currenCountDownTimer?.start()
+        tickingSoundId?.let { soundId ->
+            //soundId, 좌우 볼륨, 우선순위, loop : -1 (영원히), 재생속도 f1
+            soundPool.play(soundId, 1F, 1F, 0, -1, 1F)
+        }
+    }
+
+    private fun completeCountDown() {
+        updateRemainTimes(0)
+        updateSeekBar(0)
+        soundPool.autoPause()
+        bellSoundId?.let {  soundId ->
+            soundPool.play(soundId,1f,1f,0,-1,1f)
+        }
+    }
+
     @SuppressLint("SetTextI18n")
     private fun updateRemainTimes(remainMillis: Long){
         val remainSeconds = remainMillis/1000
